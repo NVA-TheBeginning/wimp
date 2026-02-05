ed6eb478f51a7935e5bd1a3b9c4eae4cf7b384ea
vanande <101594646+vanande@users.noreply.github.com>
2026-02-05 15:26:47 +0100

    feat: start interface (#2)
    Co-authored-by: Antony David <antonydavid945@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,6 @@
-import { createCliRenderer, TextAttributes } from "@opentui/core";
+import { createCliRenderer } from "@opentui/core";
 import { createRoot } from "@opentui/react";
-
-function App() {
-  return (
-    <box alignItems="center" justifyContent="center" flexGrow={1}>
-      <box justifyContent="center" alignItems="flex-end">
-        <ascii-font font="tiny" text="WIMP" />
-        <text attributes={TextAttributes.DIM}>Vegetable Garden Simulator</text>
-      </box>
-    </box>
-  );
-}
+import { StartScreen } from "./components/StartScreen";
 
 const renderer = await createCliRenderer();
-createRoot(renderer).render(<App />);
+createRoot(renderer).render(<StartScreen />);
