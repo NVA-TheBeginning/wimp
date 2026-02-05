74583aec1ce008dbbe849b714f4599e9eb72a90e
vanande <vkhatchatrian1@myges.fr>
2026-02-05 16:17:14 +0100

    feat: complete entities

--- src/domain/entities/crops.ts
+++ src/domain/entities/crops.ts
@@ -1,1 +1,19 @@
-export class Crops {}
+export class Crops {
+    name: string;
+    companinons: Crops[];
+    spread: number;
+    rowSpacing: number;
+    firstHarvest: number;
+    lastHarvest: number;
+    lifeSpan: number;
+
+    constructor(name: string, companinons: Crops[], spread: number, rowSpacing: number, firstHarvest: number, lastHarvest: number, lifeSpan: number) {
+        this.name = name;
+        this.companinons = companinons;
+        this.spread = spread;
+        this.rowSpacing = rowSpacing;
+        this.firstHarvest = firstHarvest;
+        this.lastHarvest = lastHarvest;
+        this.lifeSpan = lifeSpan;
+    }
+}
--- src/domain/entities/gardener.ts
+++ src/domain/entities/gardener.ts
@@ -1,1 +1,11 @@
-export class Gardener {}
+import type { Crops } from "./crops";
+
+export class Gardener {
+    name: string;
+    goal: Crops[];
+
+    constructor(name: string, goal: Crops[]) {
+        this.name = name;
+        this.goal = goal;
+    }
+}
